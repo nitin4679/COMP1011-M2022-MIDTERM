ac1a87362ce2a7099eff85a4cc1ada9bb384dadb
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-07-03 15:01:35 -0400

    created private instance members, getters and setters and constructor in Student class

--- src/main/java/ca/georgiancollege/comp1011m2022test1/Student.java
+++ src/main/java/ca/georgiancollege/comp1011m2022test1/Student.java
@@ -1,5 +1,93 @@
 package ca.georgiancollege.comp1011m2022test1;
 
 public class Student {
+    // private instance members
+    private int student;
+    private String firstName;
+    private String lastName;
+    private String telephone;
+    private String address;
+    private String province;
+    private int avgGrade;
+    private String major;
 
+    //constructor
+
+    public Student(int student, String firstName, String lastName, String telephone, String address, String province, int avgGrade, String major) {
+        this.student = student;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
+        this.address = address;
+        this.province = province;
+        this.avgGrade = avgGrade;
+        this.major = major;
     }
+
+
+
+    //getters and setters
+    public int getStudent() {
+        return student;
+    }
+
+    public void setStudent(int student) {
+        this.student = student;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public int getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(int avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
