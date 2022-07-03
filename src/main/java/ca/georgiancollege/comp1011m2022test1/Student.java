016a35d5800a646464a9ce8a544dcad87010a753
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-07-03 15:38:43 -0400

    added validation to constructor and to get/set methods

--- src/main/java/ca/georgiancollege/comp1011m2022test1/Student.java
+++ src/main/java/ca/georgiancollege/comp1011m2022test1/Student.java
@@ -1,38 +1,99 @@
 package ca.georgiancollege.comp1011m2022test1;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.matches;
+
+
 public class Student {
-    // private instance members
     private int student;
     private String firstName;
     private String lastName;
+
     private String telephone;
     private String address;
     private String province;
     private int avgGrade;
     private String major;
 
-    //constructor
-
     public Student(int student, String firstName, String lastName, String telephone, String address, String province, int avgGrade, String major) {
-        this.student = student;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.telephone = telephone;
-        this.address = address;
-        this.province = province;
-        this.avgGrade = avgGrade;
-        this.major = major;
+
+        if(student >= 200034000){
+            setStudent(student);
+        }else {
+            throw new IllegalArgumentException("Student number should be greater than 200034000!!!");
+        }
+
+        if(firstName != ""){
+
+            setFirstName(firstName);
+        }else {
+            throw new IllegalArgumentException("First and Last name can not be smaller than one character!!!");
+        }
+
+        if(lastName!= ""){
+            setLastName(lastName);
+        }else {
+
+        }
+
+        String haha = "^(?:\\(\\d{3}\\)|\\d{3})[- ]?\\d{3}[- ]?\\d{4}$\n";
+        Pattern text = Pattern.compile(haha);
+        Matcher check = text.matcher(telephone);
+        Boolean result = check.matches();
+        if(result){
+            throw new IllegalArgumentException("Telephone number not valid !!!");
+        }else {
+            setTelephone(telephone);
+        }
+
+
+        if (address.length() > 6){
+
+            setAddress(address);
+        }else {
+            throw new IllegalArgumentException("Address cannot be smaller than 6 characters!!!");
+
+        }
+
+        if (state().contains(province) ){
+
+            setProvince(province);
+        }else {
+            throw new IllegalArgumentException("Province is not valid!!!");
+
+        }
+
+        if(avgGrade >= 0 && avgGrade<=100){
+
+            setAvgGrade(avgGrade);
+        }else{
+            throw new IllegalArgumentException("Average Grade should be between 0 and 100 !!!");
+
+        }
+        if(major.length() > 5){
+
+            setMajor(major);
+        }else {
+            throw new IllegalArgumentException("Major should be greater than 5 characters !!!");
+
+        }
     }
 
 
-
-    //getters and setters
     public int getStudent() {
         return student;
     }
 
     public void setStudent(int student) {
-        this.student = student;
+        if(student >= 200034000){
+            this.student = student;
+        }else {
+            throw new IllegalArgumentException("Student number should be greater than 200034000!!!");
+        }
+
     }
 
     public String getFirstName() {
@@ -40,7 +101,12 @@ public class Student {
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if(firstName != ""){
+
+            this.firstName = firstName;
+        }else {
+            throw new IllegalArgumentException("First and Last name can not be smaller than one character!!!");
+        }
     }
 
     public String getLastName() {
@@ -48,23 +114,27 @@ public class Student {
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        if(lastName!= ""){
+            this.lastName = lastName;
+        }else {
+            throw new IllegalArgumentException("First and Last name can not be smaller than one character!!!");
+        }
     }
 
-    public String getTelephone() {
-        return telephone;
-    }
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
-        this.address = address;
+        if (address.length() > 6){
+
+            this.address = address;
+        }else {
+            throw new IllegalArgumentException("Address cannot be smaller than 6 characters!!!");
+
+        }
     }
 
     public String getProvince() {
@@ -72,7 +142,14 @@ public class Student {
     }
 
     public void setProvince(String province) {
-        this.province = province;
+        if (state().contains(province) ){
+
+            this.province = province;
+        }else {
+            throw new IllegalArgumentException("Province is not valid!!!");
+
+        }
+
     }
 
     public int getAvgGrade() {
@@ -80,7 +157,13 @@ public class Student {
     }
 
     public void setAvgGrade(int avgGrade) {
-        this.avgGrade = avgGrade;
+        if(avgGrade >= 0 && avgGrade<=100){
+
+            this.avgGrade = avgGrade;
+        }else{
+            throw new IllegalArgumentException("Average Grade should be between 0 and 100 !!!");
+
+        }
     }
 
     public String getMajor() {
@@ -88,6 +171,49 @@ public class Student {
     }
 
     public void setMajor(String major) {
-        this.major = major;
+        if(major.length() > 5){
+
+            this.major = major;
+        }else {
+            throw new IllegalArgumentException("Major should be greater than 5 characters !!!");
+
+        }
+
     }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        String haha = "^(?:\\(\\d{3}\\)|\\d{3})[- ]?\\d{3}[- ]?\\d{4}$\n";
+        Pattern text = Pattern.compile(haha);
+        Matcher check = text.matcher(telephone);
+        Boolean result = check.matches();
+        if(result){
+            throw new IllegalArgumentException("Telephone number not valid !!!");
+        }else {
+            this.telephone = telephone;
+        }
+    }
+
+    public ArrayList<String> state(){
+        ArrayList<String> provinceList = new ArrayList<String>();
+        provinceList.add("AB");
+        provinceList.add("BC");
+        provinceList.add("MB");
+        provinceList.add("NB");
+        provinceList.add("NL");
+        provinceList.add("NS");
+        provinceList.add("NT");
+        provinceList.add("NU");
+        provinceList.add("ON");
+        provinceList.add("PE");
+        provinceList.add("QC");
+        provinceList.add("SK");
+        provinceList.add("YT");
+
+        return provinceList;
+    }
+
 }
